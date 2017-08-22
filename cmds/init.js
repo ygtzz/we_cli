4ef9db243d361813d5908d22788fd2494f649f7e
ygtzz <ygtzz@126.com>
2017-08-23 00:29:28 +0800

    pretty command output

--- cmds/init.js
+++ cmds/init.js
@@ -1,6 +1,7 @@
 var exec = require('child_process').exec;
 var config = require('../templates');
 var chalk = require('chalk');
+var ora = require('ora');
 
 exports.command = 'init <projectName> <tplName>'
 exports.desc = 'init a project'
@@ -29,14 +30,15 @@ exports.handler = function (argv) {
   // git命令，远程拉取项目并自定义项目名
   var cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
 
-  console.log(chalk.white('\n Start generating...'))
+  var spinner = ora('Start generating...').start();
 
   exec(cmdStr, (error, stdout, stderr) => {
+    spinner.stop();
     if (error) {
       console.log(error)
       process.exit()
     }
-    console.log(chalk.green('\n √ Generation completed!'))
+    console.log(chalk.green('\n √ Generation success!'))
     console.log(`\n cd ${projectName} && npm install \n`)
     process.exit()
   })
--- cmds/list.js
+++ cmds/list.js
@@ -1,4 +1,5 @@
 var config = require('../templates');
+var prettyjson = require('prettyjson');
 
 exports.command = 'list'
 exports.desc = 'show all templates'
@@ -6,6 +7,13 @@ exports.builder = {
 
 }
 exports.handler = function (argv) {
-  console.log(config.tpl);
+  console.log('\ntemplate list:\n');
+  console.log(prettyjson.render(config.tpl,{
+    stringColor:'red', 
+    keysColor:'green',
+    dashColor:'yellow',
+    numberColor:'blue'
+  }));
+  console.log('');
   process.exit();
 }
